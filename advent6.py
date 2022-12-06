69c780935263414d88b443e66696dbbaa3dc05ca
DTarnu <62571630+D-Tarnu@users.noreply.github.com>
2022-12-05 21:14:34 -0800

    Add files via upload

--- advent6.py
+++ advent6.py
@@ -1,7 +1,15 @@
 f = open('advent.txt','r')
 text = f.read()
-text = text.split('\n')
 
+def differ(s):
+    for char in s:
+        if s.count(char) != 1:
+            return False
+    return True
 
+for i in range(13,len(text)):
+    if differ(text[i-13:i+1]):
+        print(i+1)
+        break
 
 f.close()
